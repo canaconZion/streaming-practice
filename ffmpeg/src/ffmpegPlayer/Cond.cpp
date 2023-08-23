c3727a36ad5a51072148d502fca262c56668f383
zion <congliu096@gmail.com>
2023-08-23 23:41:47 +0800

    feat: add audio decode thread

--- ffmpeg/src/ffmpegPlayer/Cond.cpp
+++ ffmpeg/src/ffmpegPlayer/Cond.cpp
@@ -1,4 +1,5 @@
 #include "fplayer/Cond.hpp"
+#include <iostream>
 
 Cond::Cond()
 {
@@ -9,7 +10,6 @@ Cond::Cond()
     pthread_mutex_init(&m_mutex, NULL);
     pthread_cond_init(&m_cond, NULL);
 #endif
-
 }
 
 Cond::~Cond()
@@ -20,28 +20,30 @@ Cond::~Cond()
     pthread_mutex_destroy(&m_mutex);
     pthread_cond_destroy(&m_cond);
 #endif
-
 }
 
-//加锁
+// 加锁
 int Cond::Lock()
 {
 #if defined(WIN32) && !defined(MINGW)
     EnterCriticalSection(&m_mutex);
     return 0;
 #else
+    // std::cout << "Lock\n"
+    //           << std::endl;
     return pthread_mutex_lock(&m_mutex);
 #endif
-
 }
 
-//解锁
+// 解锁
 int Cond::Unlock()
 {
 #if defined(WIN32) && !defined(MINGW)
     LeaveCriticalSection(&m_mutex);
     return 0;
 #else
+    // std::cout << "UNLock\n"
+    //           << std::endl;
     return pthread_mutex_unlock(&m_mutex);
 #endif
 }
@@ -55,23 +57,21 @@ int Cond::Wait()
 #endif
 
     return ret;
-
 }
 
-//固定时间等待
+// 固定时间等待
 int Cond::TimedWait(int second)
 {
 #if defined(WIN32) && !defined(MINGW)
-    SleepConditionVariableCS((PCONDITION_VARIABLE)&m_cond, &m_mutex, second*1000);
+    SleepConditionVariableCS((PCONDITION_VARIABLE)&m_cond, &m_mutex, second * 1000);
     return 0;
 #else
     struct timespec abstime;
-    //获取从当前时间，并加上等待时间， 设置进程的超时睡眠时间
+    // 获取从当前时间，并加上等待时间， 设置进程的超时睡眠时间
     clock_gettime(CLOCK_REALTIME, &abstime);
     abstime.tv_sec += second;
     return pthread_cond_timedwait(&m_cond, &m_mutex, &abstime);
 #endif
-
 }
 
 int Cond::Signal()
@@ -85,7 +85,7 @@ int Cond::Signal()
     return ret;
 }
 
-//唤醒所有睡眠线程
+// 唤醒所有睡眠线程
 int Cond::Broadcast()
 {
 #if defined(WIN32) && !defined(MINGW)
@@ -94,5 +94,4 @@ int Cond::Broadcast()
 #else
     return pthread_cond_broadcast(&m_cond);
 #endif
-
 }
--- ffmpeg/src/ffmpegPlayer/simple_player.cpp
+++ ffmpeg/src/ffmpegPlayer/simple_player.cpp
@@ -1,11 +1,32 @@
 #include "fplayer/simple_player.hpp"
+#include <stdio.h>
+
+static Uint8 *audio_chunk;
+static Uint32 audio_len;
+static Uint8 *audio_pos;
+
+void fill_audio(void *udata, Uint8 *stream, int len)
+{
+    // SDL 2.0
+    SDL_memset(stream, 0, len);
+    if (audio_len == 0)
+        return;
+
+    len = (len > audio_len ? audio_len : len); /*  Mix  as  much  data  as  possible  */
+
+    SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
+    audio_pos += len;
+    audio_len -= len;
+}
 
 MediaProcess::MediaProcess()
 {
     int index = 0;
-    std::cout << "----------Init decode----------------\n"
+    std::cout << "----------Init decode process----------------\n"
               << std::endl;
     sdl_init();
+    mConditon_Video = new Cond;
+    mConditon_Audio = new Cond;
 }
 
 MediaProcess::~MediaProcess()
@@ -110,18 +131,68 @@ int MediaProcess::media_decode()
         sdlRenderer = SDL_CreateRenderer(screen, -1, 0);
         sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING, pCodecCtx->width, pCodecCtx->height);
         mVideoStream = pFormatCtx->streams[videoindex];
-        // std::thread([&](MediaProcess *v_decoder)
-        //             { v_decoder->video_decode(); },
-        //             this)
-        //     .detach();
+        std::thread([&](MediaProcess *v_decoder)
+                    { v_decoder->video_decode(); },
+                    this)
+            .detach();
     }
     if (audioindex >= 0)
     {
-        std::cout << "decode audio\n"
-                  << std::endl;
+        aCodecCtx = pFormatCtx->streams[audioindex]->codec;
+        aCodec = avcodec_find_decoder(aCodecCtx->codec_id);
+        if (aCodec == NULL)
+        {
+            printf("Audio Codec not found.\n");
+            return -1;
+        }
+        if (avcodec_open2(aCodecCtx, aCodec, NULL) < 0)
+        {
+            printf("Could not open codec.\n");
+            return -1;
+        }
+        uint64_t out_channel_layout = AV_CH_LAYOUT_STEREO;
+        // nb_samples: AAC-1024 MP3-1152
+        int out_nb_samples = aCodecCtx->frame_size;
+        AVSampleFormat out_sample_fmt = AV_SAMPLE_FMT_S16;
+        int out_sample_rate = 44100;
+        int out_channels = av_get_channel_layout_nb_channels(out_channel_layout);
+        // Out Buffer Size
+        out_buffer_size = av_samples_get_buffer_size(NULL, out_channels, out_nb_samples, out_sample_fmt, 1);
+
+        a_out_buffer = (uint8_t *)av_malloc(MAX_AUDIO_FRAME_SIZE * 2);
+        aFrame = av_frame_alloc();
+        std::cout << "VideoIndex" << videoindex << "\nAudioINdex" << audioindex << std::endl;
+        wanted_spec.freq = out_sample_rate;
+        wanted_spec.format = AUDIO_S16SYS;
+        wanted_spec.channels = out_channels;
+        wanted_spec.silence = 0;
+        wanted_spec.samples = out_nb_samples;
+        wanted_spec.callback = fill_audio;
+        wanted_spec.userdata = aCodecCtx;
+
+        if (SDL_OpenAudio(&wanted_spec, NULL) < 0)
+        {
+            printf("can't open audio.\n");
+            return -1;
+        }
+        // FIX:Some Codec's Context Information is missing
+        in_channel_layout = av_get_default_channel_layout(aCodecCtx->channels);
+        // Swr
+
+        au_convert_ctx = swr_alloc();
+        au_convert_ctx = swr_alloc_set_opts(au_convert_ctx, out_channel_layout, out_sample_fmt, out_sample_rate,
+                                            in_channel_layout, pCodecCtx->sample_fmt, aCodecCtx->sample_rate, 0, NULL);
+        swr_init(au_convert_ctx);
+
+        // Play
+        SDL_PauseAudio(0);
+        std::thread([&](MediaProcess *v_decoder)
+                    { v_decoder->audio_decode(); },
+                    this)
+            .detach();
     }
-    packet = (AVPacket *)av_malloc(sizeof(AVPacket));
-    av_init_packet(packet);
+    // packet = (AVPacket *)av_malloc(sizeof(AVPacket));
+    // av_init_packet(packet);
     while (1)
     {
         // int stream_index = -1;
@@ -140,63 +211,68 @@ int MediaProcess::media_decode()
         // }
         // std::cout << "In to cycle\n"
         //           << std::endl;
-        // if (mVideoPacktList.size() > MAX_VIDEO_SIZE)
-        // {
-        //     std::this_thread::sleep_for(std::chrono::seconds(10));
-        //     continue;
-        // }
-
-        // AVPacket packet;
-        // if (av_read_frame(pFormatCtx, &packet) < 0)
-        // {
-        //     mIsReadFinished = true;
-        //     std::cout << "end of file" << std::endl;
-        //     std::this_thread::sleep_for(std::chrono::seconds(10));
-        //     goto end;
-        // }
-        // if (packet.stream_index == videoindex)
-        // {
-        //     std::cout << "Try to input video to queue\n"
-        //               << std::endl;
-        //     inputVideoQuene(packet);
-        // }
-        // else
-        // {
-        //     // Free the packet that was allocated by av_read_frame
-        //     av_packet_unref(&packet);
-        // }
-        if (av_read_frame(pFormatCtx, packet) < 0)
-            goto end;
-        if (packet->stream_index == videoindex)
+        if (mVideoPacktList.size() > MAX_VIDEO_SIZE)
         {
-            // SDL_WaitEvent(&event);
-            // if (event.type == SFM_REFRESH_EVENT)
-            // {
-            sdlRect_1.y = 2;
-            sdlRect_1.x = 2;
-            sdlRect_1.w = screen_w;
-            sdlRect_1.h = screen_h;
-            if (packet->stream_index == videoindex)
-            {
-                ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
-                if (ret < 0)
-                {
-                    std::cout << "Decode Error.\n"
-                            << std::endl;
-                    goto end;
-                }
-                if (got_picture)
-                {
-                    sws_scale(img_convert_ctx, (const unsigned char *const *)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameYUV->data, pFrameYUV->linesize);
-                    SDL_UpdateTexture(sdlTexture, NULL, pFrameYUV->data[0], pFrameYUV->linesize[0]);
-                    SDL_RenderClear(sdlRenderer);
-                    SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect_1);
-                    SDL_RenderPresent(sdlRenderer);
-                }
-            }
-            SDL_Delay(40);
-            av_free_packet(packet);
+            std::this_thread::sleep_for(std::chrono::seconds(10));
+            continue;
         }
+        AVPacket packet;
+        if (av_read_frame(pFormatCtx, &packet) < 0)
+        {
+            mIsReadFinished = true;
+            std::cout << "end of file" << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(10));
+            goto end;
+        }
+        if (packet.stream_index == videoindex)
+        {
+            std::cout << "Try to input video to queue\n"
+                      << std::endl;
+            inputVideoQuene(packet);
+        }
+        else if (packet.stream_index == audioindex)
+        {
+            std::cout << "Try to input audio to queue\n"
+                      << std::endl;
+            inputAudioQuene(packet);
+        }
+        else
+        {
+            // Free the packet that was allocated by av_read_frame
+            av_packet_unref(&packet);
+        }
+        // if (av_read_frame(pFormatCtx, packet) < 0)
+        //     goto end;
+        // if (packet->stream_index == videoindex)
+        // {
+        //     // SDL_WaitEvent(&event);
+        //     // if (event.type == SFM_REFRESH_EVENT)
+        //     // {
+        //     sdlRect_1.y = 2;
+        //     sdlRect_1.x = 2;
+        //     sdlRect_1.w = screen_w;
+        //     sdlRect_1.h = screen_h;
+        //     if (packet->stream_index == videoindex)
+        //     {
+        //         ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
+        //         if (ret < 0)
+        //         {
+        //             std::cout << "Decode Error.\n"
+        //                     << std::endl;
+        //             goto end;
+        //         }
+        //         if (got_picture)
+        //         {
+        //             sws_scale(img_convert_ctx, (const unsigned char *const *)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameYUV->data, pFrameYUV->linesize);
+        //             SDL_UpdateTexture(sdlTexture, NULL, pFrameYUV->data[0], pFrameYUV->linesize[0]);
+        //             SDL_RenderClear(sdlRenderer);
+        //             SDL_RenderCopy(sdlRenderer, sdlTexture, NULL, &sdlRect_1);
+        //             SDL_RenderPresent(sdlRenderer);
+        //         }
+        //     }
+        //     SDL_Delay(40);
+        //     av_free_packet(packet);
+        // }
     }
 end:
     // clearAudioQuene();
@@ -296,15 +372,10 @@ int MediaProcess::video_decode()
             SDL_RenderPresent(sdlRenderer);
         }
         SDL_Delay(40);
-        av_free_packet(packet);
+        // av_free_packet(packet); // 已弃用
         av_packet_unref(packet);
     }
-    swr_free(&au_convert_ctx);
 
-    av_free(a_out_buffer);
-    avcodec_close(aCodecCtx);
-
-    av_free(out_buffer);
     av_frame_free(&pFrameYUV);
     av_frame_free(&pFrame);
     avcodec_close(pCodecCtx);
@@ -313,6 +384,62 @@ int MediaProcess::video_decode()
 
 int MediaProcess::audio_decode()
 {
+    while (1)
+    {
+        std::cout << "Begin to decode audio packet" << std::endl;
+        mConditon_Audio->Lock();
+        if (mAudioPacktList.size() <= 0)
+        {
+            mConditon_Audio->Unlock();
+            if (mIsReadFinished)
+            {
+                // 队列里面没有数据了且读取完毕了
+                std::cout << "Read packet finished, quit process\n"
+                          << std::endl;
+                break;
+            }
+            else
+            {
+                std::cout << "No audio packet in queue\n"
+                          << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1)); // 队列只是暂时没有数据而已
+                continue;
+            }
+        }
+        AVPacket pkt1 = mAudioPacktList.front();
+        mAudioPacktList.pop_front();
+
+        mConditon_Audio->Unlock();
+
+        AVPacket *packet = &pkt1;
+        ;
+        ret = avcodec_decode_audio4(aCodecCtx, aFrame, &a_got_picture, packet);
+        if (ret < 0)
+        {
+            printf("Error in decoding audio frame.\n");
+            return -1;
+        }
+        if (a_got_picture > 0)
+        {
+            swr_convert(au_convert_ctx, &a_out_buffer, MAX_AUDIO_FRAME_SIZE, (const uint8_t **)pFrame->data, pFrame->nb_samples);
+            printf("index:%5d\t pts:%lld\t packet size:%d\n", index, packet->pts, packet->size);
+            index++;
+        }
+
+        while (audio_len > 0) // Wait until finish
+            SDL_Delay(1);
+
+        // Set audio buffer (PCM data)
+        audio_chunk = (Uint8 *)a_out_buffer;
+        // Audio buffer length
+        audio_len = out_buffer_size;
+        audio_pos = audio_chunk;
+        av_packet_unref(packet);
+    }
+    av_free(a_out_buffer);
+    av_frame_free(&aFrame);
+    avcodec_close(aCodecCtx);
+    swr_free(&au_convert_ctx);
 }
 
 int MediaProcess::sdl_init()
@@ -364,6 +491,22 @@ bool MediaProcess::inputVideoQuene(const AVPacket &pkt)
     return true;
 }
 
+bool MediaProcess::inputAudioQuene(const AVPacket &pkt)
+{
+    if (av_dup_packet((AVPacket *)&pkt) < 0)
+    {
+        return false;
+    }
+
+    mConditon_Audio->Lock();
+    mAudioPacktList.push_back(pkt);
+    mConditon_Audio->Signal();
+    mConditon_Audio->Unlock();
+    std::cout << "inputVideoQuene: successful" << std::endl;
+
+    return true;
+}
+
 void MediaProcess::clearVideoQuene()
 {
     mConditon_Video->Lock();
@@ -373,4 +516,15 @@ void MediaProcess::clearVideoQuene()
     }
     mVideoPacktList.clear();
     mConditon_Video->Unlock();
+}
+
+void MediaProcess::clearAudioQuene()
+{
+    mConditon_Audio->Lock();
+    for (AVPacket pkt : mAudioPacktList)
+    {
+        av_packet_unref(&pkt);
+    }
+    mAudioPacktList.clear();
+    mConditon_Audio->Unlock();
 }
